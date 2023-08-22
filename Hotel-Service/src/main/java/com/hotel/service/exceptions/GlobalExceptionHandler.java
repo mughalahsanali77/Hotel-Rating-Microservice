4c4a77080e5a32be63afa1f0cfda2ad8765703af
Ahsan Ali Mughal <mughalahsanali77@gmail.com>
2023-08-22 21:04:59 +0500

    Updated GlobalExceptionHandler

--- Hotel-Service/src/main/java/com/hotel/service/exceptions/GlobalExceptionHandler.java
+++ Hotel-Service/src/main/java/com/hotel/service/exceptions/GlobalExceptionHandler.java
@@ -7,13 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+/*
+*
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse> handlerResourceNotFoundException(ResourceNotFoundException ex){
         String message=ex.getMessage();
         ApiResponse response=ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
         return new ResponseEntity<ApiResponse>(response,HttpStatus.NOT_FOUND);
+    }
+*/
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String,Object>> handlerResourceNotFoundException(ResourceNotFoundException ex){
+        Map map=new HashMap();
+        map.put("message",ex.getMessage());
+        map.put("success",false);
+        map.put("status",HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
     }
 }
